9cac0f310baa336940624d849d43368ae1b7ab8c
laroccacharly <larocca.charly@gmail.com>
2025-07-04 09:30:27 -0400

    sample_without_replacement

--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,8 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 #include "gurobi_c++.h"
+#include <random>
+#include <algorithm>
 
 using namespace std;
 using namespace fmt;
@@ -36,6 +38,36 @@ inline bool isBinary(GRBVar& var) {
     }
     
     return false;
+}
+
+// Sample without replacement from the set {0, 1, ..., n-1}
+// Returns a vector of sample_size integers sampled uniformly without replacement
+inline vector<int> sample_without_replacement(int n, int sample_size, mt19937& rng) {
+
+    // Create vector with elements {0, 1, ..., n-1}
+    vector<int> population;
+    population.reserve(n);
+    for (int i = 0; i < n; i++) {
+        population.push_back(i);
+    }
+    
+    // Shuffle the entire vector
+    shuffle(population.begin(), population.end(), rng);
+    
+    // Take the first sample_size elements
+    population.resize(sample_size);
+    return population;
+}
+
+inline vector<int> sample_percentage(int n, double percentage, mt19937& rng) {
+    int sample_size = static_cast<int>(n * percentage);
+    return sample_without_replacement(n, sample_size, rng);
+}
+
+inline vector<int> sample_percentage(int n, double percentage) {
+    random_device rd;
+    mt19937 rng(rd());
+    return sample_percentage(n, percentage, rng);
 }
 
 // A solution is a vector of indices for the non zero variables 
