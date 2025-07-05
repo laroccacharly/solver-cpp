60406482d71454064589144ca36b1196d509cb0e
laroccacharly <larocca.charly@gmail.com>
2025-07-05 10:04:44 -0400

    update ui with pages

--- ui.py
+++ ui.py
@@ -8,20 +8,38 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 
-st.title("Database Tables")
+def tables_page(): 
+    st.title("Database Tables")
 
-conn = sqlite3.connect("data/db.sqlite")
+    conn = sqlite3.connect("data/db.sqlite")
 
-# Get a list of tables
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
+    # Get a list of tables
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
 
-# Display each table
-for table_name in tables:
-    table_name = table_name[0]
-    st.subheader(f"Table: {table_name}")
-    df = pd.read_sql_query(f"SELECT * FROM {table_name} LIMIT 1000", conn)
-    st.dataframe(df)
+    # Display each table
+    for table_name in tables:
+        table_name = table_name[0]
+        st.subheader(f"Table: {table_name}")
+        df = pd.read_sql_query(f"SELECT * FROM {table_name} LIMIT 1000", conn)
+        st.dataframe(df)
 
-conn.close()
+    conn.close()
+
+
+def home_page(): 
+    st.title("Home")
+
+
+def main_ui(): 
+    st.set_page_config(page_title="Solver", page_icon=":robot:", layout="wide")
+    st.sidebar.title("Navigation")
+    page = st.sidebar.selectbox("Select a page", ["Home", "Tables"], index=0)
+    if page == "Home":
+        home_page()
+    elif page == "Tables":
+        tables_page()
+
+if __name__ == "__main__":
+    main_ui()
