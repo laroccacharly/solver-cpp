ce51c028ee67f7c8ee495d655c5707d59ce518e1
laroccacharly <larocca.charly@gmail.com>
2025-07-06 12:48:21 -0400

    small fixes in env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,12 @@
 alias init="vcpkg new --application"
 export PROJECT_NAME="solver-cpp"
 alias addfmt="vcpkg add port fmt"
-export PROJECT_NAME="solver-cpp"
 
 # Gurobi setup
 export GUROBI_HOME=/Library/gurobi1200/macos_universal2
 echo "GUROBI_HOME set to: $GUROBI_HOME"
 # MPS files directory
-export MPS_FILES_DIR='/Users/charly/.miplib_benchmark/mps_files'
+export MPS_FILES_DIR="$HOME/.miplib_benchmark/mps_files"
 
 alias preset='cmake --preset=vcpkg' 
 alias build='cmake --build build'
