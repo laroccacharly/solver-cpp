418422caa18bad73c22a3658209412a0171116a1
laroccacharly <larocca.charly@gmail.com>
2025-07-07 09:45:18 -0400

    clean up pages

--- src/pysolver/pages.py
+++ src/pysolver/pages.py
@@ -1,8 +1,6 @@
 from typing import Callable
 from pydantic import BaseModel
 from pysolver.primal_gap_results import primal_gap_results_ui
-from pysolver.warm_start import warm_start_ui
-from .instance_selection import instance_selection_ui
 from .all_tables import all_tables_ui
 from .primal_gap_results import primal_gap_results_ui
 from .run_sql import run_sql_ui
@@ -13,10 +11,10 @@ class Page(BaseModel):
 
 
 def get_pages() -> list[Page]: 
+    from .instance_selection import instance_selection_ui
     return [
         Page(name="Instance Selection", function=instance_selection_ui),
-        Page(name="Warm Start", function=warm_start_ui),
         Page(name="Primal Gap Results", function=primal_gap_results_ui),
         Page(name="Tables", function=all_tables_ui),
-        Page(name="Run SQL", function=run_sql_ui),
+        # Page(name="Run SQL", function=run_sql_ui),
     ]
