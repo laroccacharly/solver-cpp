1be360b9936e726158c844b74aeaeff7812d3a49
laroccacharly <larocca.charly@gmail.com>
2025-07-07 09:45:37 -0400

    update env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,17 @@
 alias init="vcpkg new --application"
-export PROJECT_NAME="solver-cpp"
 alias addfmt="vcpkg add port fmt"
 
 # Gurobi setup
 export GUROBI_HOME=/Library/gurobi1200/macos_universal2
 echo "GUROBI_HOME set to: $GUROBI_HOME"
-# MPS files directory
+# MPS files 
 export MPS_FILES_DIR="$HOME/.miplib_benchmark/mps_files"
+alias setup_miplib="uv run setup_miplib.py"
 
+export PROJECT_NAME="solver-cpp"
 alias preset='cmake --preset=vcpkg' 
 alias build='cmake --build build'
-alias run='./build/$PROJECT_NAME'
+alias run="./build/$PROJECT_NAME"
 alias brun="build && run"
 alias clean='rm -rf build'
 alias r="build && run -a"
@@ -19,4 +20,6 @@ alias r="build && run -a"
 # python scripts 
 alias showdb="uv run show_sqlite.py"
 alias ui="uv run streamlit run ui.py"
-alias select="uv run select_instances.py"
+alias select="uv run python -m src.pysolver.make_instance_selection"
+alias metrics="uv run python -m src.pysolver.compute_metrics"
+alias instances="uv run instances_with_binary_variables.py"
