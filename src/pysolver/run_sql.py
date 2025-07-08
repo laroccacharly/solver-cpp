b7e8e651242bde802180ffbc118091ad285e33b1
laroccacharly <larocca.charly@gmail.com>
2025-07-08 08:34:36 -0400

    update run_sql_ui

--- src/pysolver/run_sql.py
+++ src/pysolver/run_sql.py
@@ -1,14 +1,17 @@
 import streamlit as st
-from .connection import get_connection, close_connection
+from .connection import query_with_duckdb
+
+def get_example_sql() -> str:
+    return """
+    SELECT * FROM instances LIMIT 10
+    """
 
 def run_sql_ui():
     st.subheader("Run SQL")
-    sql = st.text_area("SQL")
+
+    example_sql = get_example_sql()
+
+    sql = st.text_area("SQL", example_sql, placeholder="Enter your SQL query here...")
     if st.button("Run"):
-        con = get_connection()
-        cursor = con.cursor()
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        st.dataframe(results)
-        con.commit()
-        close_connection()
+        df = query_with_duckdb(sql)
+        st.dataframe(df)
